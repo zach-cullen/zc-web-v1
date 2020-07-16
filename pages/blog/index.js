c0c17acb4b1894c6e367334a6c46244200feacea
Zach Cullen <zacullen@gmail.com>
2020-07-16 14:35:16 -0500

    display links to blog post with dynamic routes

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,10 +1,24 @@
 import Header from '../../components/header'
+import Link from 'next/link'
 
 export default () => {
   return (
     <>
       <Header />
       <h1>Blog</h1>
+      <h2>Posts</h2>
+      <ul>
+        <li>
+          <Link as='blog/1' href='/blog/[id]'>
+            <a>First Post</a>
+          </Link>
+        </li>
+        <li>
+          <Link as='blog/2' href='/blog/[id]'>
+            <a>Second Post</a>
+          </Link>
+        </li>
+      </ul>
     </>
   )
 }
