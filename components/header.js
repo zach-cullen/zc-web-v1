53cacc0ce15608480d7c1ab1f4c8c7ae475f602c
Zach Cullen <zacullen@gmail.com>
2020-07-16 13:49:05 -0500

    create simple static about and blog page to make header links functional

--- components/header.js
+++ components/header.js
@@ -1,9 +1,15 @@
+import Link from 'next/link';
+
 export default () => (
   <header>
-    <ul>
-      <li>Home</li>
-      <li>About</li>
-      <li>Blog</li>
-    </ul>
+    <Link href='/'>
+      <a>Home</a>
+    </Link>
+    <Link href='/about'>
+      <a>About</a>
+    </Link>
+    <Link href='/blog'>
+      <a>Blog</a>
+    </Link>
   </header>
 )
---
+++ pages/blog.js
@@ -0,0 +1,10 @@
+import Header from '../components/header'
+
+export default () => {
+  return (
+    <>
+      <Header />
+      <h1>Blog</h1>
+    </>
+  )
+}
