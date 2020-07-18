3eb73e56a451d9c6494fcc54565b1afc7af03c25
Zach Cullen <zacullen@gmail.com>
2020-07-18 17:11:10 -0500

    reformat fetch link to single quotes

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -26,9 +26,8 @@ const Blog = ({ posts }) => {
 }
 
 Blog.getInitialProps = async () => {
-  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const res = await fetch('https://jsonplaceholder.typicode.com/posts')
   const posts = await res.json()
-
   return { posts }
 }
 
