e0cf99623ddc859791843d1a345aa01a7e373c57
Zach Cullen <zacullen@gmail.com>
2020-07-19 16:24:37 -0500

    replace h1 with chakra heading as h1

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Header from '../components/header'
+import { Heading } from '@chakra-ui/core'
 
 export default () => {
   return (
     <>
       <Header />
-      <h1>Hi, I'm Zach Cullen</h1>
+      <Heading as='h1' size='xl'>Hi, I'm Zach Cullen</Heading>
       <p>
         A Software Engineer with a background in video production and web design. Join me as I explore the frontiers of modern web app development using React & JavaScript.
       </p>
