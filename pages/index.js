8e407f2f4d92922039639a6b9ce905acb17bb53f
Zach Cullen <zacullen@gmail.com>
2020-07-19 16:36:40 -0500

    wrap page in box with margin to contain and center content

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Header from '../components/header'
-import { Heading } from '@chakra-ui/core'
+import { Heading, Box } from '@chakra-ui/core'
 
 export default () => {
   return (
-    <>
+    <Box maxW='960px' m='0 auto'>
       <Header />
       <Heading as='h1' size='xl'>Hi, I'm Zach Cullen</Heading>
       <p>
         A Software Engineer with a background in video production and web design. Join me as I explore the frontiers of modern web app development using React & JavaScript.
       </p>
-    </>
+    </Box>
   )
 }
