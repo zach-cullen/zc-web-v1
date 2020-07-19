abbf6f986ca53f24740db462d2549eb06958ea64
Zach Cullen <zacullen@gmail.com>
2020-07-19 17:35:15 -0500

    provide flex box container for nav justifying logo and links to start / end

--- components/header.js
+++ components/header.js
@@ -1,16 +1,28 @@
 import NextLink from 'next/link'
-import { Link } from '@chakra-ui/core'
+import { Flex, Box, Heading, Link } from '@chakra-ui/core'
 
 export default () => (
   <header>
-    <NextLink href='/'>
-      <Link>Home</Link>
-    </NextLink>
-    <NextLink href='/about'>
-      <Link>About</Link>
-    </NextLink>
-    <NextLink href='/blog'>
-      <Link>Blog</Link>
-    </NextLink>
+    <Flex
+      maxW='720px'
+      m='0 auto'
+      py='4'
+      justify='space-between'
+    >
+      <Box area='logo'>
+        <Heading as='h5' fontSize='2xl'>Zc</Heading>
+      </Box>
+      <Box area='links'>
+        <NextLink href='/'>
+          <Link>Home</Link>
+        </NextLink>
+        <NextLink href='/about'>
+          <Link>About</Link>
+        </NextLink>
+        <NextLink href='/blog'>
+          <Link>Blog</Link>
+        </NextLink>
+      </Box>
+    </Flex>
   </header>
 )
