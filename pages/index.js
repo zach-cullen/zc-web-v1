eafae4d583c5cff5b555e6ef1555000e0119bfbc
Zach Cullen <zacullen@gmail.com>
2020-07-19 17:35:39 -0500

    place header outside of containing box

--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,14 @@ import { Heading, Box } from '@chakra-ui/core'
 
 export default () => {
   return (
-    <Box maxW='960px' m='0 auto'>
+    <>
       <Header />
-      <Heading as='h1' size='xl'>Hi, I'm Zach Cullen</Heading>
-      <p>
-        A Software Engineer with a background in video production and web design. Join me as I explore the frontiers of modern web app development using React & JavaScript.
-      </p>
-    </Box>
+      <Box maxW='720px' m='0 auto'>
+        <Heading as='h1' size='xl'>Hi, I'm Zach Cullen</Heading>
+        <p>
+          A Software Engineer with a background in video production and web design. Join me as I explore the frontiers of modern web app development using React & JavaScript.
+        </p>
+      </Box>
+    </>
   )
 }
