7fc54332bd7b592289b43aec75b20992f8e28ca9
Zach Cullen <zacullen@gmail.com>
2020-07-21 10:50:40 -0500

    add projects and posts sections to home page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Header from '../components/Header'
-import { Heading } from '@chakra-ui/core'
+import { Heading, Text } from '@chakra-ui/core'
 import PageSection from '../components/PageSection'
 
 export default () => {
@@ -8,9 +8,19 @@ export default () => {
       <Header />
       <PageSection>
         <Heading as='h1' size='xl'>Hi, I'm Zach Cullen</Heading>
-        <p>
+        <Text my='4'>
           A Software Engineer with a background in video production and web design. Join me as I explore the frontiers of modern web app development using React & JavaScript.
-        </p>
+        </Text>
+      </PageSection>
+      <PageSection sectionBg='gray.50'>
+        <Heading as='h2' size='lg'>
+          Projects
+        </Heading>
+      </PageSection>
+      <PageSection>
+        <Heading as='h2' size='lg'>
+          Posts
+        </Heading>
       </PageSection>
     </>
   )
