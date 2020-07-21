9a9e42b71bffaf7604c9af973ed4b6b665345c6a
Zach Cullen <zacullen@gmail.com>
2020-07-21 09:25:52 -0500

    use PageSection to maintain width consistency with page contents

--- components/header.js
+++ components/header.js
@@ -1,28 +1,28 @@
 import NextLink from 'next/link'
 import { Flex, Box, Heading, Link } from '@chakra-ui/core'
+import PageSection from './PageSection'
 
 export default () => (
   <header>
-    <Flex
-      maxW='720px'
-      m='0 auto'
-      py='4'
-      justify='space-between'
-    >
-      <Box area='logo'>
-        <Heading as='h5' fontSize='2xl'>Zc</Heading>
-      </Box>
-      <Box area='links'>
-        <NextLink href='/'>
-          <Link>Home</Link>
-        </NextLink>
-        <NextLink href='/about'>
-          <Link>About</Link>
-        </NextLink>
-        <NextLink href='/blog'>
-          <Link>Blog</Link>
-        </NextLink>
-      </Box>
-    </Flex>
+    <PageSection>
+      <Flex
+        justify='space-between'
+      >
+        <Box area='logo'>
+          <Heading as='h5' fontSize='2xl'>Zc</Heading>
+        </Box>
+        <Box area='links'>
+          <NextLink href='/'>
+            <Link>Home</Link>
+          </NextLink>
+          <NextLink href='/about'>
+            <Link>About</Link>
+          </NextLink>
+          <NextLink href='/blog'>
+            <Link>Blog</Link>
+          </NextLink>
+        </Box>
+      </Flex>
+    </PageSection>
   </header>
 )
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,17 @@
-import Header from '../components/header'
-import { Heading, Box } from '@chakra-ui/core'
+import Header from '../components/Header'
+import { Heading } from '@chakra-ui/core'
+import PageSection from '../components/PageSection'
 
 export default () => {
   return (
     <>
       <Header />
-      <Box maxW='720px' m='0 auto'>
+      <PageSection>
         <Heading as='h1' size='xl'>Hi, I'm Zach Cullen</Heading>
         <p>
           A Software Engineer with a background in video production and web design. Join me as I explore the frontiers of modern web app development using React & JavaScript.
         </p>
-      </Box>
+      </PageSection>
     </>
   )
 }
